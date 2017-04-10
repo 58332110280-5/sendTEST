8fcecb9cb94b533aad25f1032a78536edffb18d9
Your Name58332110280-5 <0016053@gmail.com>
2017-04-10 13:03:43 +0700

    SEND

---
+++ src/Send.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Send {
+
+	public static void main(String[] args) {
+		ArrayList<Object> lst = new ArrayList(1);
+		lst.add(0,1);
+		lst.add(1,2);
+		lst.add(2,3);
+		lst.add(3,4);
+		for (int i = 0 ; i<lst.size() ; i++);{
+			if((Integer)lst.get(i) % 2 == 0 ){
+				lst.remove(i);
+	}
+			System.out.println(tostring);
+}
