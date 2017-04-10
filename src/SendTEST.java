0b1790d2641465ad53abd9d1655102813bd2a273
Your Name58332110280-5 <0016053@gmail.com>
2017-04-10 13:07:14 +0700

    SENT

--- src/SendTEST.java
+++ src/SendTEST.java
@@ -1,22 +1,17 @@
 import java.util.ArrayList;
 
-public class Send {
+public class SendTEST {
 
 	public static void main(String[] args) {
 		ArrayList<Object> lst = new ArrayList(1);
-		lst.add(0,1);
-		lst.add(1,2);
-		lst.add(2,3);
-		lst.add(3,4);
-		lst.add(4,5);
-		lst.add(5,6);
-		lst.add(6,7);
-		lst.add(7,8);
-		for (int i = 0 ; i<lst.size(i) ; i++);{
-			if((Integer)lst.get(i) % 2 == 0 ){
-				lst.remove(i);
+		lst.add(0, 1);
+		lst.add(1, 2);
+		lst.add(2, 3);
+		lst.add(3, 4);
+		lst.add(4, 5);
+		lst.add(5, 6);
+		lst.add(6, 7);
+		lst.add(7, 8);
 	}
-		}
-			System.out.println(tostring);
 
 }
